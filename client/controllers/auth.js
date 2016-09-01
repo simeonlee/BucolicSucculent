5bfe2127492d51ca0440089e23854bd51bdbf4f9
Hack Reactor Students <accounts@hackreactor.com>
2016-09-01 13:00:45 -0700

    Fix view functionality

---
+++ client/controllers/auth.js
@@ -0,0 +1,17 @@
+angular.module('app.auth', [])
+
+.controller('AuthController', function( $scope ) {
+  console.log('hello from controller')
+  $scope.user = {};
+
+  $scope.login = function() {
+
+  };
+
+  $scope.signup = function() {
+
+  };
+  
+
+
+  })
