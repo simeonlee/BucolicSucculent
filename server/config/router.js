09c0196a74990ffa14f82adb027131057eae8b1d
Derek Liu <liu.derekt@gmail.com>
2016-09-01 20:38:29 -0700

    Lay out basic routing structure on server.

--- server/config/router.js
+++ server/config/router.js
@@ -7,16 +7,48 @@ module.exports = function(app, express) {
 
   app.get('/api/game', function (req, res) {
 
-    res.send('Hello World! This is the game route.');
+    res.send('This is the GET for /game');
+
   });
 
-  app.get('/api/game/map', function (req, res) {
+  app.post('/api/game', function (req, res) {
 
-    res.send('Hello World! This is the game map.');
+    res.send('This is the POST for /game');
+
   });
 
-  app.post('/api/users', function(req, res) {
+  app.post('/api/game/create', function (req, res) {
+    //This is when the creator makes a game and clicks create game
+    var creator = 'sam';
+    //somehow we create the code;
+    var pathUrl = 'abcde';
 
+    var locations = [
+      {lat: 1, lng: 1},
+      {lat: 2, lng: 3},
+      {lat: 4, lng: 5},
+      {lat: 6, lng: 7}
+    ];
+
+    User.find({
+      where: {
+        username: creator
+      }
+    }).then(function(user) {
+      Game.create({
+        creatorId: user.id,
+        path: pathUrl,
+    }).then(function(game) {
+      // iterate through locations(waypoints)
+    });
+
+
+
+    res.send('Should send url(path) to game here');
+
+  });
+
+  app.post('/api/users/signup', function(req, res) {
     var username = req.body.username.toLowerCase();
     var password = req.body.password;
 
@@ -27,5 +59,13 @@ module.exports = function(app, express) {
     .catch(function(err) {
       res.status(409).send('Username already exists');
     });
+  });
+
+  app.post('/api/users/login', function(req, res) {
+    // Login users
+
+    res.send('Should pass');
+
+
   });
 };
