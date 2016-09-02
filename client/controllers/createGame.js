dd23a29f0297dd5bf96e7cfd70399ab47aa13bbc
Brian Kilrain <bkilrain@gmail.com>
2016-09-01 19:23:52 -0700

    Add marker functionality on maps

--- client/controllers/createGame.js
+++ client/controllers/createGame.js
@@ -1,4 +1,4 @@
-angular.module('app.createGame', [])
+angular.module('app.createGame', ['uiGmapgoogle-maps'])
 
 .controller('createGameController', function($scope, uiGmapGoogleMapApi) {
   $scope.submitWaypoints = function() {
@@ -9,7 +9,20 @@ angular.module('app.createGame', [])
   $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
 
   uiGmapGoogleMapApi.then(function(maps) { //<------- create map (promise) and after
-
+    $scope.createOptions = {
+      drawingMode: google.maps.drawing.OverlayType.MARKER,
+      markerOptions: {
+        draggable: true
+      },
+      drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: [
+          google.maps.drawing.OverlayType.MARKER,
+        ]
+    },
+    }
   });
+
+
 
 })
