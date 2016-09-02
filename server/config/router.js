eea23bed75072a051d19ab4b0eb2b4356ab3b6f3
Derek Liu <liu.derekt@gmail.com>
2016-09-01 19:11:21 -0700

    Refactor schemas to seperate folders and add POST route to create new users at /users.

--- server/config/router.js
+++ server/config/router.js
@@ -1,11 +1,34 @@
+var Game = require('./db-config').Game;
+var Location = require('./db-config').Location;
+var Status = require('./db-config').Status;
+var User = require('./db-config').User;
+
 module.exports = function(app, express) {
 
   app.get('/api/game', function (req, res) {
+
     res.send('Hello World! This is the game route.');
   });
 
   app.get('/api/game/map', function (req, res) {
+
     res.send('Hello World! This is the game map.');
   });
 
+  app.post('/api/users', function(req, res) {
+
+    var username = req.body.username.toLowerCase();
+    var password = req.body.password;
+
+    User.create({username: username, password: password})
+    .then(function() {
+      res.status(201).send('New user added.');
+    })
+    .catch(function(err) {
+      res.status(409).send('Username already exists');
+    });
+
+    console.log('REQUEST', req.body);
+
+  });
 };
--- server/games/games.js
+++ server/games/games.js
@@ -1,5 +1,7 @@
+var Sequelize = require('sequelize');
+
 module.exports = function(db) {
-  var Game = db.define('game', {
+  var Game = db.define('games', {
     creatorId: Sequelize.INTEGER,
     path: Sequelize.STRING
   });
