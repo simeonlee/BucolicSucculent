338ce9e6faa9ca151bb6ed8b7c8f54c2dd90fcf1
Brian Kilrain <bkilrain@gmail.com>
2016-09-06 10:20:43 -0700

    Fix bug with $watch in game controller

--- client/controllers/game.js
+++ client/controllers/game.js
@@ -1,4 +1,7 @@
 angular.module('app.game', ['uiGmapgoogle-maps', 'app.services', 'ngGeolocation'])
+.run(function(){
+
+})
 
 .controller('gameController', function($scope, data) {
   $scope.map = data.map;
@@ -18,18 +21,16 @@ angular.module('app.game', ['uiGmapgoogle-maps', 'app.services', 'ngGeolocation'
       timeout: 60000,
       maximumAge: 250,
       enableHighAccuracy: true
-    })
+  })
   $scope.myPosition = $geolocation.position;
 
-  $scope.$watch('myPosition', function (newValue, oldValue) {
-    if (newValue.coords) {
-      console.log(newValue)
-      $scope.circle = {
+  $scope.$on('$geolocation.position.changed', function(newValue) {
+    $scope.circle = {
         center: {
-          latitude: newValue.coords.latitude,
-          longitude: newValue.coords.longitude
+          latitude: $geolocation.position.coords.latitude,
+          longitude: $geolocation.position.coords.longitude
         },
-        radius: newValue.coords.accuracy,
+        radius: $geolocation.position.coords.accuracy,
         stroke: {
           color: 'blue',
           weight: 1,
@@ -41,17 +42,15 @@ angular.module('app.game', ['uiGmapgoogle-maps', 'app.services', 'ngGeolocation'
           opacity: 0.3
         }
       };  
-      $scope.myLatLng = new google.maps.LatLng(newValue.coords.latitude, newValue.coords.longitude);                    
-    }
-  }, true);
-
+      $scope.myLatLng = new google.maps.LatLng($geolocation.position.coords.latitude, $geolocation.position.coords.longitude);  
+  })
 
   $scope.user = {"name": 'Brian', "locations": [{"id": 1, "status": true},{"id": 2, "status": false},{"id": 3, "status": false}]};  //<--------- TODO: figure out matching logged-in user with appropriate user from game data
 
   $scope.validateLocation = function(locationId) { //TODO: validate location with browser geolocation api
     console.log(locationId)
     // get lat and lng from locationId
-    var point2 = new google.maps.LatLng(37.5728027, -122.3356122); //<-- Dummy point... loc to be checked
+    var point2 = new google.maps.LatLng(37.7837646, -122.40909429999999); //<-- Dummy point... loc to be checked
     
     var distanceBetween = google.maps.geometry.spherical.computeDistanceBetween($scope.myLatLng, point2);
     console.log(distanceBetween)
