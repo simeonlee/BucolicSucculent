cc3ddd1f0c61cc22f1b5c19d1ec5a67ae1bd42ac
Derek Liu <liu.derekt@gmail.com>
2016-09-08 15:02:27 -0700

    Change example user to 'derek' with known token value.

--- specs/server/ServerSpec.js
+++ specs/server/ServerSpec.js
@@ -12,23 +12,23 @@ var User = require('../../server/config/db-config').User;
 describe ('', function() {
 
   beforeEach(function() {
-    // User.destroy({
-    //   where: {
-    //     username: 'beth'
-    //   }
-    // });
+    User.destroy({
+      where: {
+        username: 'derek'
+      }
+    });
   });
 
   describe('POST /api/users/signup', function() {
     it('should create a user', function(done) {
       request(app)
         .post('/api/users/signup')
-        .set('username', 'beth')
-        .set('password', 'beth')
+        .set('username', 'derek')
+        .set('password', 'derek')
         .expect(function() {
-          User.findOne({ where: { 'username': 'beth' } })
+          User.findOne({ where: { 'username': 'derek' } })
             .then(function(user) {
-              expect(user.username).to.equal('beth');
+              expect(user.username).to.equal('derek');
             });
         })
         .end(done);
@@ -51,10 +51,4 @@ describe ('', function() {
 
   //   })
   // })
-  // describe('POST /api/users/login', function() {
-  //   it('should login a user', function(done) {
-
-  //   })
-  // })
 });
-
