e4fd3134a19f06d85657978bdde609de4f146c6c
Derek Liu <liu.derekt@gmail.com>
2016-09-08 01:48:03 -0700

    Add 2nd test case for user signup.

--- specs/server/ServerSpec.js
+++ specs/server/ServerSpec.js
@@ -12,11 +12,11 @@ var User = require('../../server/config/db-config').User;
 describe ('', function() {
 
   beforeEach(function() {
-    User.destroy({
-      where: {
-        username: 'beth'
-      }
-    });
+    // User.destroy({
+    //   where: {
+    //     username: 'beth'
+    //   }
+    // });
   })
 
   describe('POST /api/users/signup', function() {
@@ -32,6 +32,23 @@ describe ('', function() {
             });
         })
         .end(done);
+    })
+
+    it('Successful signup logs in a new user', function(done) {
+      request(app)
+        .post('/api/users/signup')
+        .set('username', 'beth')
+        .set('password', 'beth')
+        .expect(function(res) {
+          expect(res.headers.location).to.equal('/#/createGame');
+        })
+        .end(done);
+    })
+  })
+
+  describe('POST /api/users/login', function() {
+    it('should login a user', function(done) {
+
     })
   })
 
