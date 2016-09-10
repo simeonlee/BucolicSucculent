c616f79fe6fae982fa9befecd35139e21f6c164d
Derek Liu <liu.derekt@gmail.com>
2016-09-10 13:32:36 -0700

    Add onDelete option to Associations to allow cascade deletes.

--- server/config/db-config.js
+++ server/config/db-config.js
@@ -17,16 +17,16 @@ var Location = require('../locations/locations')(db);
 var Status = require('../status/status')(db);
 var User = require('../users/users')(db);
 
-User.belongsToMany(Game, {through: 'usergame', foreignKey: 'userId'}); // on delete, cascade
+User.belongsToMany(Game, {through: 'usergame', foreignKey: 'userId', onDelete: 'CASCADE'}); // on delete, cascade
 Game.belongsToMany(User, {through: 'usergame', foreignKey: 'gameId'});
 
 Location.belongsToMany(User, {through: 'statuses', foreignKey: 'locationId'});
-User.belongsToMany(Location, {through: 'statuses', foreignKey: 'userId'}); // on delete, cascade 
+User.belongsToMany(Location, {through: 'statuses', foreignKey: 'userId', onDelete: 'CASCADE'}); // on delete, cascade 
 
 Game.belongsTo(User, {foreignKey: 'creatorId', as: 'creator'});
 
 Location.belongsTo(Game);
-Game.hasMany(Location); // on delete, cascade
+Game.hasMany(Location, {onDelete: 'CASCADE'}); // on delete, cascade
 
 module.exports = {
   db: db,
