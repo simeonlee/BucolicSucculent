14522094b5838044ea877ec88b48b86f4c31f300
Derek Liu <liu.derekt@gmail.com>
2016-09-10 15:26:20 -0700

    Fix cascade delete definitions for Creator/Game/Location relations.

--- server/config/db-config.js
+++ server/config/db-config.js
@@ -17,16 +17,16 @@ var Location = require('../locations/locations')(db);
 var Status = require('../status/status')(db);
 var User = require('../users/users')(db);
 
-User.belongsToMany(Game, {through: 'usergame', foreignKey: 'userId', onDelete: 'CASCADE'}); // on delete, cascade
+User.belongsToMany(Game, {through: 'usergame', foreignKey: 'userId'});
 Game.belongsToMany(User, {through: 'usergame', foreignKey: 'gameId'});
 
 Location.belongsToMany(User, {through: 'statuses', foreignKey: 'locationId'});
-User.belongsToMany(Location, {through: 'statuses', foreignKey: 'userId', onDelete: 'CASCADE'}); // on delete, cascade 
+User.belongsToMany(Location, {through: 'statuses', foreignKey: 'userId'});
 
-Game.belongsTo(User, {foreignKey: 'creatorId', as: 'creator'});
+Game.belongsTo(User, {foreignKey: { name: 'creatorId' }, as: 'creator', onDelete: 'cascade'}); // on User delete, cascade delete all games User created
 
-Location.belongsTo(Game);
-Game.hasMany(Location, {onDelete: 'CASCADE'}); // on delete, cascade
+Location.belongsTo(Game, { onDelete: 'cascade' }); // on Game delete, cascade delete all Locations in that game
+Game.hasMany(Location); 
 
 module.exports = {
   db: db,
