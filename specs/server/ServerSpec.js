7c423b1946aedd333756ae28509b72387b886680
Derek Liu <liu.derekt@gmail.com>
2016-09-11 03:47:09 -0700

    Add tests for GET requests to /api/game with both username and path.

--- specs/server/ServerSpec.js
+++ specs/server/ServerSpec.js
@@ -127,77 +127,142 @@ xdescribe ('Signup/Login for Users', function() {
   })
 });
 
-describe('New Game Creation', function() {
+describe('Game Creation', function() {
 
   var token;
   var user;
   var pathUrl;
-  var locations = {
-    markers: [
-      { latitude: 1.23, longitude: 2.34, sequence: 1},
-      { latitude: 3.45, longitude: 4.56, sequence: 2},
-      { latitude: 5.67, longitude: 6.78, sequence: 3},
-      { latitude: 7.89, longitude: 8.90, sequence: 4} ]
-  };
 
   before(function(done) {
-    request(app)
-      .post('/api/users/signup')
-      .set('username', 'test1')
-      .set('password', 'test1')
-      .expect(201)
-      .expect(function(res) {
-        token = res.body.token;
-        user = res.body.user;
-      })
-      .end(done)
+    User.destroy({ where: { username: 'test1' } })
+    .then(function() {
+      request(app)
+        .post('/api/users/signup')
+        .set('username', 'test1')
+        .set('password', 'test1')
+        .expect(201)
+        .expect(function(res) {
+          token = res.body.token;
+          user = res.body.user;
+        })
+        .end(done)
+    })
   });
 
   after(function() {
     User.destroy({ where: { username: 'test1' } });
   });
 
-  xdescribe('POST request /api/game/create', function() {
-    it('create a new game with valid credentials', function(done) {
+  describe('POST request /api/game/create', function() {
+
+    var locations = {
+      markers: [
+        { latitude: 1.35, longitude: 2.46, sequence: 1},
+        { latitude: 3.57, longitude: 4.68, sequence: 2} ]
+    };
+
+    before(function(done) {
       request(app)
         .post('/api/game/create')
         .set('username', user)
         .set('X-ACCESS-TOKEN', token)
         .send(locations)
         .end(function(err, res) {
-
-          expect(res.text).to.exist;
           pathUrl = res.text.substring(res.text.length-9, res.text.length-4);
+          done();
+        })
+    });
 
-          Game.findOne({ where: { path: pathUrl } })
-          .then(function(gameFound) {
-            expect(gameFound).to.exist;
-            return Location.findAll({ include: { model: Game, where: { path: pathUrl } }});
-          })
-          .then(function(locationsFound) {
-            expect(locationsFound).to.exist;
-            expect(locationsFound.length).to.equal(4);
-          })
-          .then(done)
-          .catch(function(err) { throw err; });
-        });
+    it('should create a new game in the db', function(done) {
+      Game.findOne({ where: { path: pathUrl } })
+      .then(function(gameFound) {
+        expect(gameFound).to.exist;
+        done();
+      });
+    });
+
+    it('should create a new location for each marker', function(done) {
+      Location.findAll({
+        include: {
+          model: Game,
+          where: { path: pathUrl }
+        },
+        raw: true
+      }).then(function(result) {
+        expect(result.length).to.equal(2);
+        expect(result[0].latitude).to.equal(1.35);
+        expect(result[0].longitude).to.equal(2.46);
+        expect(result[0].sequence).to.equal(1);
+        expect(result[1].latitude).to.equal(3.57);
+        expect(result[1].longitude).to.equal(4.68);
+        expect(result[1].sequence).to.equal(2);
+        done();
+      });
     });
   });
 
   describe('GET request /api/game', function() {
 
+    var locations = {
+      markers: [
+        { latitude: 4.56, longitude: 5.67, sequence: 1},
+        { latitude: 6.78, longitude: 7.89, sequence: 2} ]
+    };
+
+    before(function(done) {
+      request(app)
+        .post('/api/game/create')
+        .set('username', user)
+        .set('X-ACCESS-TOKEN', token)
+        .send(locations)
+        .end(function(err, res) {
+          pathUrl = res.text.substring(res.text.length-9, res.text.length-4);
+          done();
+        });
+    })
+
     describe('When given both the game Path and the Username', function() {
-      it('generates Statuses for each Location if initial join', function() {
 
+      before(function(done) {
+        request(app)
+          .get('/api/game')
+          .set('username', user)
+          .set('X-ACCESS-TOKEN', token)
+          .query({ path: pathUrl })
+          .query({ username: 'test1' })
+          .end(function(err, res) {
+            done();
+          })
       });
-      it('initializes all Statuses as false', function() {
 
+      it('generates Statuses for each Location', function(done) {
+        Location.findAll({
+          include: [{
+            model: User,
+            where: { username: 'test1' }
+          },{
+            model: Game,
+            where: { path: pathUrl }
+          }],
+          raw: true
+        }).then(function(result) {
+          expect(result.length).to.equal(2);
+          done();
+        });
       });
-      it('returns the statuses when player has joined the game', function() {
 
-      });
-      it('returns the saved statuses if game has been previously joined', function() {
-
+      it('returns the statuses for players who joined the game', function(done) {
+        request(app)
+          .get('/api/game')
+          .set('username', user)
+          .set('X-ACCESS-TOKEN', token)
+          .query({ path: pathUrl })
+          .query({ username: 'test1' })
+          .end(function(err, res) {
+            expect(res.body.locations[0].statuses.status).to.equal(false);
+            expect(res.body.locations[1].statuses.status).to.equal(false);
+            done();
+          })
       });
     });
 
