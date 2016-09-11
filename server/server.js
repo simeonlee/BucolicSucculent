66ef43164e1831343b6a92348f2f3df66defe2c0
Derek Liu <liu.derekt@gmail.com>
2016-09-11 15:20:19 -0700

    Clean up dependencies.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,9 @@
 var express = require('express');
-var jwt = require('jwt-simple');
-var mysql = require('mysql');
 var bodyParser = require('body-parser');
+
 var app = express();
 
-//hi
 app.use(bodyParser.json());
-var parser = bodyParser.json();
 app.use(express.static('./dist'));
 
 // initialize tokens
@@ -14,7 +11,6 @@ app.set('jwtTokenSecret', 'teambsAThackreactor47');
 
 var db = require('./config/db-config').db;
 require('./config/router')(app, express);
-
 
 app.listen(4200, function () {
   console.log('Server listening on port 4200!');
