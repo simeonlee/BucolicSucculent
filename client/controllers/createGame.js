2db9b6e5b0eac1ea858bbc631b5fc8b1e5caad14
Simeon Lee <simeonhlee@gmail.com>
2016-09-13 19:57:52 -0700

    Implement nearby search and map click events system

--- client/controllers/createGame.js
+++ client/controllers/createGame.js
@@ -53,6 +53,7 @@ angular.module('app.createGame', ['uiGmapgoogle-maps', 'app.services', 'app'])
 
   $scope.showMap = true;
 
+<<<<<<< 2e83d073d0aba51de1610d100202e5362b457762
   $scope.map = {
     // Default San Francisco
     center: {
@@ -314,15 +315,29 @@ angular.module('app.createGame', ['uiGmapgoogle-maps', 'app.services', 'app'])
     templateUrl: '/views/infowindow.html'
 
 =======
+=======
+>>>>>>> Implement nearby search and map click events system
   $scope.map = {
-    center: { 
-      latitude: 37.7836881,                 //<------- Default SF map
+    // Default San Francisco
+    center: {
+      latitude: 37.7836881,                 
       longitude: -122.4090401 
     }, 
-    zoom: 13
+    zoom: 14,
+    events: {
+      tilesloaded: function (map, eventName, originalEventArgs) {
+        // Once map is loaded and ready, this callback is hit
+      },
+      click: function (mapModel, eventName, originalEventArgs) {
+        var e = originalEventArgs[0];
+        $scope.findNearbyPlaces(e.latLng);
+      }
+    },
+    markers: []
   };
 >>>>>>> Apply minor comment and styling cleanups to createGame.js
 
+<<<<<<< 2e83d073d0aba51de1610d100202e5362b457762
 <<<<<<< 24c6309bb5f667642d963aedb5baedccb470c532
     // template: '<div class="iw">Name: {{data.name}} Address: {{data.photo}}</div>'
     // template: '<div class="iw">\
@@ -336,12 +351,48 @@ angular.module('app.createGame', ['uiGmapgoogle-maps', 'app.services', 'app'])
     //   //    $compile(jqLiteWrappedElement)(scope);
     //   // })
 =======
+=======
+  // Find nearby places using Google API based on location
+  // https://developers.google.com/maps/documentation/javascript/places#place_search_requests
+  $scope.findNearbyPlaces = function(latLng) {
+    var request = {
+      location: latLng,
+      radius: '50', // meters
+      types: ['establishment']
+    };
+    service = new google.maps.places.PlacesService(document.createElement('div'));
+    service.nearbySearch(request, function(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        console.log(results);
+        var sequence = 0;
+        for (var i = 0; i < 5; i++) {
+          var place = results[i];
+          var marker = {
+            sequence: sequence,
+            latitude: place.geometry.location.lat(),
+            longitude: place.geometry.location.lng(),
+            markerOptions: {
+              // visible: false,
+              animation: google.maps.Animation.BOUNCE
+            }
+          };
+          $scope.map.markers.push(marker);
+          sequence++;
+        }
+        // Apply changes to digest loop in order to render labeled markers
+        $scope.$apply();
+      }
+    });
+  }
+
+>>>>>>> Implement nearby search and map click events system
   $scope.searchbox = {
     // GET template from when we saved in $templateCache in app.js
     template: 'searchbox.tpl.html',
-    position: 'top-center',
+    position: 'TOP_RIGHT',
     options: {
       autocomplete: true,
+      types: ['point_of_interest', 'establishment']
     },
     events: {
       place_changed: function (autocomplete){
@@ -397,6 +448,7 @@ angular.module('app.createGame', ['uiGmapgoogle-maps', 'app.services', 'app'])
     }
   }
 
+<<<<<<< 2e83d073d0aba51de1610d100202e5362b457762
   $scope.map.markers = []; //<--------- save marker coords here
 >>>>>>> Add searchbox in createGame and searchbox template in app.js
 
@@ -462,3 +514,53 @@ angular.module('app.createGame', ['uiGmapgoogle-maps', 'app.services', 'app'])
 //   });
 // }]);
 // >>>>>>> Apply minor comment and styling cleanups to createGame.js
+// =======
+//   // Create map (promise) and after
+//   uiGmapGoogleMapApi.then(function() { 
+
+//     // // API marker creation tool
+//     // $scope.createOptions = {
+//     //   drawingMode: google.maps.drawing.OverlayType.MARKER,
+//     //   drawingControlOptions: {
+//     //     position: google.maps.ControlPosition.TOP_CENTER,
+//     //     drawingModes: [
+//     //       google.maps.drawing.OverlayType.MARKER,
+//     //     ]
+//     //   },
+//     //   markerOptions: {
+//     //     visible: false,
+//     //     animation: google.maps.Animation.BOUNCE
+//     //   },
+//     // };
+
+//     // When user clicks on map to find locations, use API to find nearby places of interest
+//     // var el = document.getElementsByClassName('angular-google-map');
+//     // console.log(el);
+//     // var map = angular.element(document.getElementsByClassName('angular-google-map')[1]);
+//     // console.log(document.getElementsByClassName('angular-google-map')[1]);
+//     // google.maps.event.addListener(document.getElementsByClassName('angular-google-map')[1], 'click', function(e) {
+//     //   console.log(e.latLng);
+//     //   $scope.findNearbyPlaces(e.latLng);
+//     // });
+//   });
+
+//   // call when game is to be created
+//   $scope.submitWaypoints = function() {
+
+//     // get user for ajax request
+//     $scope.user = $window.localStorage.getItem('user');
+//     // get waypoints from map and submit to server to create createGame
+//     Requests.createGame($scope.user, $scope.map.markers)
+//       .then(function(res) {
+
+//         // get back hashed game url and display
+//         var gameUrl = res.data;
+//         $scope.gamePath = gameUrl; //<------ to be game url
+//       });
+
+//       // hide map on game creation
+//       $scope.showMap = false;
+
+//   };
+// }]);
+// >>>>>>> Implement nearby search and map click events system
